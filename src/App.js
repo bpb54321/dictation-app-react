cbb51e5952ceea9ee3a8ae7144d5859052e7c586
Brian Blosser <bpb54321@gmail.com>
2018-08-15 10:33:14 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="dictation-app">
+        <header className="header">
+          <h1 className="header__heading">Dictation App</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
